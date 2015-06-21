838019c88cf5574da02928e5bf7d854d78260a8a
Shmidt Kirill <shmidtk@gmail.com>
2015-06-21 15:03:31 +0600

    updated r file

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## This two functions calculating inverse matrix. Functions optimize computation
+## by saving inverse matrix in cache. 
+## This function create a specail list of functions, witch we use to save data 
+## to cache. Its make no computation on data.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL   
+        set <- function(y) { ## This function set m to NULL in global 
+                             ## enviroment, and it can change x if we want
+            x <<- y 
+            m <<- NULL 
+        }
+        get <- function() x ## this function just return x
+        setsolve <- function(solve) m <<- solve ## This function set m to solve 
+                                                ## in global enviroment
+        getsolve <- function() m ## this function just return m
+        list(set=set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
+    ## Result is four functions
 }
 
 
-## Write a short comment describing this function
+## Function make computations of invert matrix of take data from cache. 
+## Its use vector from function makeCacheMatrix as input.
+
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getsolve() ## we use getsolve() function witch retrun us m. 
+                          ## For the first time it will me NULL, for other times 
+                          ## it will be solved matrix
+        if(!is.null(m)) {
+            message("getting cached data")
+            return(m) ## if we have solved matrix in cache we exit from function
+        }
+        
+        data <- x$get() ## if we dont have solves matrix we compute it
+        m <- solve(data, ...)
+        x$setsolve(m) ## and put data in cache by this function
+        m
 }
+        ## Return a matrix that is the inverse of 'x'
+
